41f688d8783e6126ff3d5674836812b78a1395eb
nenadnesta <nenadnesta@gmail.com>
2019-05-01 16:43:29 +0200

    Action Results and Action Parameters.

--- GameGale/Controllers/GamesController.cs
+++ GameGale/Controllers/GamesController.cs
@@ -1,20 +1,59 @@
 ﻿using GameGale.Models;
+using System;
 using System.Web.Mvc;
 
 namespace GameGale.Controllers
 {
     public class GamesController : Controller
     {
-        // GET: Games
-        public ActionResult Index()
-        {
-            return View();
-        }
-
+        /************ACTION RESULTS*******************/
+        
+        //ActionResult is the base class for all action classes in ASP.NET MVC https://www.c-sharpcorner.com/article/action-result-in-asp-net-mvc/
+        //Depending what the action does it will return an instance of one of the classes that derives from ActionResult
+        //ViewResult,PartialViewResult,ContentResult,RedirectResult,RedirectToRouteResult,JsonResult,FileResult,HttpNotFoundResult,EmptyResult
         public ActionResult Random()
         {
             var game = new Game() { Name = "Metro Exodus" };
+            //return new ViewResult(game); 
+            //View() - helper mathod that derives from base Controller class
             return View(game);
+
+            //return Content("Hello World!");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+        }
+
+        /************ACTION PARAMETERS*******************/
+
+        //GET: Games/Edit/1
+        //GET: Games/Edit?id=1
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        //GET: Games/Edit?id=1 - error
+        //GET: Games/Edit/1 - error. Gore je moglo jer je id default ovde ne moze
+        //GET: Games/Edit?gameId=1 - ok
+        /*
+        public ActionResult Edit(int gameId)
+        {
+            return Content("id=" + gameId);
+        }
+        */
+
+        //GET: Games
+        //GET: Games?pageIndex=2
+        //GET: Games?pageIndex=2&sortBy=ReleaseDate
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex ={0}&sortBy={1}", pageIndex, sortBy));
         }
     }
 }
