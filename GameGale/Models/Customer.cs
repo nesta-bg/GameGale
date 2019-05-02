f098c346ebbded65d2c3b349404d542c46247137
nenadnesta <nenadnesta@gmail.com>
2019-05-02 18:46:01 +0200

    Add Birthdate to Customer Model.

--- GameGale/Models/Customer.cs
+++ GameGale/Models/Customer.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace GameGale.Models
 {
@@ -15,5 +16,7 @@ namespace GameGale.Models
         public MembershipType MembershipType { get; set; }
 
         public byte MembershipTypeId { get; set; }
+
+        public DateTime? Birthdate { get; set; }
     }
 }
