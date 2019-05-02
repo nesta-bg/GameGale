c926f27b886f1eebf85264220e3775d1420a3eb3
nenadnesta <nenadnesta@gmail.com>
2019-05-02 15:32:07 +0200

    Load Customers from the Database.

--- GameGale/Controllers/CustomersController.cs
+++ GameGale/Controllers/CustomersController.cs
@@ -7,30 +7,38 @@ namespace GameGale.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext(); 
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Customers
         public ActionResult Index()
         {
-            var customers = GetCustomers();
+            //Deferred Execution(ef doesn't gonna query the db)
+            //var customers = _context.Customers;
+
+            //Immediate Execution
+            var customers = _context.Customers.ToList();
+
             return View(customers);
         }
 
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
                 return HttpNotFound();
 
             return View(customer);
-        }
-
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-            {
-                new Customer { Id = 1, Name = "Johnny Depp"},
-                new Customer { Id = 2, Name = "Steve Jobs"}
-            };
         }
     }
 }
